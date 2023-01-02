fd74ff776b99bf50c741e845d48898e75f2af8fc
Sina Madadi <sinamadadi.work@gmail.com>
2023-01-02 17:53:03 +0330

    register added

--- src/KhodAmooz.jsx
+++ src/KhodAmooz.jsx
@@ -3,12 +3,14 @@ import Courses from './components/common/Courses';
 import MainLayout from './components/Layouts/MainLayout';
 import Login from './components/Login/Login';
 import { Route, Switch } from 'react-router-dom'
+import Register from './components/Login/Register';
 
 const KhodAmooz = (props) => {
     return (
         <MainLayout>
             <Switch>
                 <Route path='/login' component={Login} />
+                <Route path='/register' component={Register} />
                 <Route path='/' exact component={Courses} />
             </Switch>
         </MainLayout>
