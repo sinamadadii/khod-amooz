9d537c16dbeba40b82562822351f8102dfabd408
Sina Madadi <sinamadadi.work@gmail.com>
2023-01-08 00:09:43 +0330

    user login added !

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,6 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { toast } from 'react-toastify';
+import { loginUser } from '../../services/userService';
 
 const Login = (props) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async event => {
+        event.preventDefault();
+        const user = { email, password };
+
+        const reset = () => {
+            setEmail('');
+            setPassword('');
+        }
+
+        try {
+            const { status, data } = await loginUser(user);
+            if (status === 200) {
+                toast.success('ورود موفق', {
+                    position: 'top-right',
+                    closeOnClick: true
+                });
+                console.log(data);
+                reset();
+            }
+
+        } catch (error) {
+            console.log(error);
+            toast.error('ورود نا موفق', {
+                position: 'top-right',
+                closeOnClick: true
+            });
+        }
+    }
+
     return (
         <main className="client-page">
             <div className="container-content">
@@ -9,16 +43,30 @@ const Login = (props) => {
 
                 <div className="form-layer">
 
-                    <form action="" method="">
+                    <form onSubmit={handleSubmit} action="" method="">
 
                         <div className="input-group">
                             <span className="input-group-addon" id="email-address"><i className="zmdi zmdi-email"></i></span>
-                            <input type="text" className="form-control" placeholder="ایمیل" aria-describedby="email-address" />
+                            <input
+                                type="text"
+                                className="form-control"
+                                placeholder="ایمیل"
+                                aria-describedby="email-address"
+                                value={email}
+                                onChange={e => setEmail(e.target.value)}
+                            />
                         </div>
 
                         <div className="input-group">
                             <span className="input-group-addon" id="password"><i className="zmdi zmdi-lock"></i></span>
-                            <input type="text" className="form-control" placeholder="رمز عبور " aria-describedby="password" />
+                            <input
+                                type="text"
+                                className="form-control"
+                                placeholder="رمز عبور "
+                                aria-describedby="password"
+                                value={password}
+                                onChange={e => setPassword(e.target.value)}
+                            />
                         </div>
 
                         <div className="remember-me">
