dafdbe6fa9f7ded1b8dfd27c03b4a468c56915ac
Sina Madadi <sinamadadi.work@gmail.com>
2023-05-20 16:59:36 +0330

    forgoted

--- src/components/common/Courses.jsx
+++ src/components/common/Courses.jsx
@@ -4,35 +4,6 @@ import axios from 'axios';
 
 const Courses = () => {
 
-    const perosn = () => {
-        const res = axios
-            // .get(`https://jsonplaceholder.typicode.com/posts`)
-            .get(`http://localhost:4000/users`)
-            .then((response) => {
-                return response
-            })
-            .catch((err) => {
-                return err.response
-            })
-        return res
-    }
-    const [posts, setpost] = useState([]);
-    useEffect(() => {
-        // console.log("object");
-        perosn().then((response) => {
-            if (response.status === 200) {
-                setpost(response.data)
-            }
-            console.log(response.data);
-            // console.log("obaject");
-        })
-        // fetch('https://jsonplaceholder.typicode.com/todos/1')
-        //     .then(response => response.json())
-        //     .then(json => console.log(json))
-    }, [])
-
-
-
     return (<section className="terms-items">
         <header>
             <h2> آخرین دوره های خودآموز </h2>
@@ -40,17 +11,84 @@ const Courses = () => {
         </header>
         <div className="row">
 
-            {posts.map((post, i) => (
-                <div key={i} className="col-lg-3 col-md-4 col-sm-6 col-xs-12 term-col">
-                    <article>
-                        <NavLink to="/course/1" className="img-layer"><img src="images/pic/1.jpg" /></NavLink>
-                        <h2><NavLink to="/course/1"> fdd </NavLink></h2>
-                        <span> رایگان </span>
-                        <i>1:52:32</i>
-                    </article>
-                </div>
+            <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12 term-col">
+                <article>
+                    <NavLink to="/course" className="img-layer"><img src="images/pic/1.jpg" /></NavLink>
+                    <h2><NavLink to="/course"> آموزش متریال دیاین در زامارین </NavLink></h2>
+                    <span> رایگان </span>
+                    <i>1:52:32</i>
+                </article>
+            </div>
 
-            ))}
+
+            <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12 term-col">
+                <article>
+                    <NavLink to="/course" className="img-layer"><img src="images/pic/2.jpg" /></NavLink>
+                    <h2><NavLink to="/course"> آموزش متریال دیاین در زامارین </NavLink></h2>
+                    <span> رایگان </span>
+                    <i>1:52:32</i>
+                </article>
+            </div>
+
+
+            <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12 term-col">
+                <article>
+                    <NavLink to="/course" className="img-layer"><img src="images/pic/3.jpg" /></NavLink>
+                    <h2><NavLink to="/course"> آموزش متریال دیاین در زامارین </NavLink></h2>
+                    <span> 150.000 تومان </span>
+                    <i>1:52:32</i>
+                </article>
+            </div>
+
+
+            <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12 term-col">
+                <article>
+                    <NavLink to="/course" className="img-layer"><img src="images/pic/4.jpg" /></NavLink>
+                    <h2><NavLink to="/course"> آموزش متریال دیاین در زامارین </NavLink></h2>
+                    <span> رایگان </span>
+                    <i>1:52:32</i>
+                </article>
+            </div>
+
+
+            <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12 term-col">
+                <article>
+                    <NavLink to="/course" className="img-layer"><img src="images/pic/5.jpg" /></NavLink>
+                    <h2><NavLink to="/course"> آموزش متریال دیاین در زامارین </NavLink></h2>
+                    <span> 15.000 تومان </span>
+                    <i>1:52:32</i>
+                </article>
+            </div>
+
+
+            <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12 term-col">
+                <article>
+                    <NavLink to="/course" className="img-layer"><img src="images/pic/6.jpg" /></NavLink>
+                    <h2><NavLink to="/course"> آموزش متریال دیاین در زامارین </NavLink></h2>
+                    <span> رایگان </span>
+                    <i>1:52:32</i>
+                </article>
+            </div>
+
+
+            <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12 term-col">
+                <article>
+                    <NavLink to="/course" className="img-layer"><img src="images/pic/7.jpg" /></NavLink>
+                    <h2><NavLink to="/course"> آموزش متریال دیاین در زامارین </NavLink></h2>
+                    <span> 20.000 تومان </span>
+                    <i>1:52:32</i>
+                </article>
+            </div>
+
+
+            <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12 term-col">
+                <article>
+                    <NavLink to="/course" className="img-layer"><img src="images/pic/8.jpg" /></NavLink>
+                    <h2><NavLink to="/course"> آموزش متریال دیاین در زامارین </NavLink></h2>
+                    <span> 75.000 تومان </span>
+                    <i>1:52:32</i>
+                </article>
+            </div>
 
         </div>
     </section>);
